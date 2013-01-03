d335a1fe56f1c887aed34d7a50cba65947077c74
pszName <pszName@gmail.com>
2013-01-03 15:45:50 +0100

    Test Layer

---
+++ src/de/fhk/mps/fhMahJongg/Layer.scala
@@ -0,0 +1,9 @@
+package de.fhk.mps.fhMahJongg
+
+class Layer(_cbLayer: Int, _iXmax: Int, _iYmax: Int) {
+  
+	def Test(i: Int):Int =
+	{
+	  i
+	}
+}
---
+++ test/de/fhk/mps/fhMahJongg/LayerSpec.scala
@@ -0,0 +1,13 @@
+package de.fhk.mps.fhMahJongg
+
+import org.specs2.mutable._
+
+class LayerSpec extends SpecificationWithJUnit{
+  
+	"" should{
+	  val Layer = new Layer(0,0,0)
+	  "" in{
+	    
+	  }
+	}
+}
