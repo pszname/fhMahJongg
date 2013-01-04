b49725e4fc9d27142c474ed350d5e1616735c500
DonCanalie <doncanalie2@web.de>
2013-01-04 13:33:47 +0100

    Layer updated and javadoc

--- src/de/fhk/mps/fhMahJongg/Layer.scala
+++ src/de/fhk/mps/fhMahJongg/Layer.scala
@@ -18,7 +18,12 @@ class Layer(private var m_iXmax: Int, private var m_iYmax: Int) {
 	
 	// Defs =============================================================================
 	
-	//returns TRUE, if position is available, else FALSE
+	/**
+	 * sets a tile id to a certain position
+	 * 
+	 * @param a unique ID and the position as x-intercept and y-intercept
+	 * @return <code>true</code>, if the position is available and set, otherwise <code>false</code>
+	*/
 	def setTileIDToPosition(id: Int, x: Int, y: Int): Boolean	= {
 	  if (Matrix(x)(y) == 0)	{	
 	    Matrix(x)(y) = id
@@ -27,10 +32,27 @@ class Layer(private var m_iXmax: Int, private var m_iYmax: Int) {
 	  else	return false
 	}
 	
+	/**
+	 * deletes a TileID from xy-position
+	 * 
+	 * @param x-intercept, y-intercept
+	 */
 	def deleteTileIDFromPosition(x: Int, y: Int) { Matrix(x)(y) = 0 }
-			
+		
+	/**
+	 * returns a TileID from xy-position
+	 * 
+	 * @param x-intercept, y-intercept
+	 * @return TileID as <code>Int</code>
+	 */
 	def getIDFromPosition(x: Int, y: Int): Int = Matrix(x)(y)
 	
+	/**
+	 * returns the xy-position in addition to a TileID
+	 * 
+	 * @param TileID
+	 * @return xy-position as <code>Vector(Int, Int)</code>
+	 */
 	def getPositionFromID(id: Int): Vector[Int] =	{
 	  for (i <- 0 until m_iXmax; j <- 0 until m_iYmax)	{
 	    if (Matrix(i)(j) == id) return Vector(i, j)
@@ -38,5 +60,10 @@ class Layer(private var m_iXmax: Int, private var m_iYmax: Int) {
 	  return Vector(0, 0)
 	}
 	
+	/**
+	 * returns the the whole layer as field
+	 * 
+	 * @return layer as <code>Array[Array[Int]]</code>
+	 */
 	def getField: Array[Array[Int]] = Matrix
 }
